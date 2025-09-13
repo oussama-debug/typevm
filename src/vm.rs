0421537e2959d93065fe4ca54ac7862425aa44ac
Oussama <oussama@jaaouani.com>
2025-09-12 21:53:08 -0400

    update

---
+++ src/vm.rs
@@ -0,0 +1,90 @@
+enum MachineRegister {
+    A,
+    B,
+    SP,
+    PC,
+    BP,
+    FLAGS,
+}
+
+trait Addressable {
+    fn read(&self, address: u16) -> Option<u8>;
+    fn write(&mut self, address: u16, value: u8) -> bool;
+
+    fn read16(&self, address: u16) -> Option<u16> {
+        if let Some(x0) = self.read(address) {
+            if let Some(x1) = self.read(address + 1) {
+                return Some((x0 as u16) | ((x1 as u16) << 8));
+            }
+        }
+        None
+    }
+
+    fn write16(&mut self, address: u16, value: u16) -> bool {
+        let lower = value & 0xff;
+        let upper = (value & 0xff00) >> 8;
+
+        return self.write(address, lower as u8) && self.write(address + 1, upper as u8);
+    }
+
+    fn copy(&mut self, from: u16, to: u16, size: usize) -> bool {
+        for i in 0..size {
+            if let Some(x) = self.read((from + (i as u16)) as u16) {
+                if !self.write((to + (i as u16)) as u16, x) {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+}
+
+struct MachineMemory {
+    bytes: Vec<u8>,
+    size: usize,
+}
+
+impl MachineMemory {
+    pub fn new(u: usize) -> Self {
+        Self {
+            bytes: vec![0; u],
+            size: u,
+        }
+    }
+}
+
+impl Addressable for MachineMemory {
+    fn read(&self, address: u16) -> Option<u8> {
+        self.bytes.get(address as usize).copied()
+    }
+
+    fn write(&mut self, address: u16, value: u8) -> bool {
+        if address < self.size as u16 {
+            self.bytes[address as usize] = value;
+            true
+        } else {
+            false
+        }
+    }
+}
+
+struct MachineDefinition {
+    registers: [u16; 8],
+    memory: MachineMemory,
+}
+
+impl MachineDefinition {
+    pub fn new() -> Self {
+        Self {
+            registers: [0; 8],
+            memory: MachineMemory::new(5000),
+        }
+    }
+
+    pub fn step(&mut self) -> Result<(), &'static str> {
+        let pc = self.registers[MachineRegister::PC as usize] as usize;
+        let instruction = self.memory.read16(pc.try_into().unwrap()).unwrap();
+        println!("{} @ {}", instruction, pc);
+        Ok(())
+    }
+}
